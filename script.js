bfb8f1630191870054f5354ca41aa88f1775c720
Maxwilguentz-cmd <maxwilguentz@gmail.com>
2026-06-29 02:49:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function salut(){
+    alert("bonjour wilguentz");
+}
+function tiktok(){
+    window.open("https://www.tiktok.com","_blank");
+}
+function jeux(){
+        window.location.href="page-de-jeux.html"
+
+}
+function streaming(){
+    window.location.href="page-de-streaming.html"
+}
+function cartedebit(){
+    window.location.href="page-de-giftcard.html"
+}
+function netflix(){
+    window.location.href="Netflix.html"
+}
