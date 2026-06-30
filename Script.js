8d104287511a0c8ffb7a1b08297d34f2530f009f
Maxwilguentz-cmd <maxwilguentz@gmail.com>
2026-06-30 17:18:22 -0400

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,32 @@
+
+function salut(){
+    alert("sevis sa poko disponib");
+}
+function tiktok(){
+    window.open("https://www.tiktok.com","_blank");
+}
+function jeux(){
+        window.location.href="page-de-jeux.html"
+
+}
+function streaming(){
+    window.location.href="page-de-streaming.html"
+}
+function cartedebit(){
+    window.location.href="page-de-giftcard.html"
+}
+function netflix(){
+    window.location.href="Netflix.html"
+}
+function chek(){
+    window.location.href="checkout.html"
+}
+function chek2(){
+    window.location.href="checkout2.html"
+}
+function chekdisney-premium(){
+    window.location.href="checkout-disney-premium.html"
+}
+
+function chekdisney(){
+    Window.location.href="checkout-disney.html”}
